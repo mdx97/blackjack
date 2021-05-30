483cab9a53ff55eb137e5466aee22471fc5583ca
mdx97 <mathewhorner456@gmail.com>
2021-05-30 17:06:09 -0500

    Updates

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,15 @@ const HELP_MENU_HEADER: [&str; 4] = [
     "-----------------------",
     "Command Line Blackjack",
     "-----------------------",
-    "Available commands",
+    "Available commands:",
 ];
+const PROMPT: &str = ">>>";
 
 type Card = (String, String);
 type Hand = Vec<Card>;
 type Deck = Hand;
 
+#[derive(PartialEq)]
 enum GamePhase {
     OutOfGame,
     InGame,
@@ -30,6 +32,14 @@ fn print_lines(lines: Vec<&str>) {
 fn print_help_menu(commands: Vec<&str>) {
     print_lines(HELP_MENU_HEADER.to_vec());
     print_lines(commands);
+}
+
+fn print_hand(hand: &Hand) {
+    println!("(down) {}", get_card_name(&hand[0]));
+    for card in hand[1..].iter() {
+        println!("(up) {}", get_card_name(card));
+    }
+    println!("\nYour hand has a value of {}!", get_hand_value(hand));
 }
 
 fn build_deck(rng: &mut ThreadRng) -> Deck {
@@ -47,8 +57,9 @@ fn get_hand_value(hand: &Hand) -> u32 {
     let mut total = 0;
     for (value, _) in hand {
         let mut value = (CARD_VALUES.iter().position(|v| v.eq(value)).unwrap() as u32) + 1;
-        if value == 1 { value = 14; }
         if value > 10 { value = 10; }
+        // TODO: Handle the case where if we are busting, Aces can be worth 1 instead.
+        if value == 1 { value = 11; }
         total += value;
 
     }
@@ -66,10 +77,21 @@ fn main() {
     let mut input_buffer = String::new();
     let mut phase = GamePhase::OutOfGame;
     let mut hand = Vec::new();
+    let mut bet = 0;
 
     loop {
+        if phase == GamePhase::OutOfGame && chips == 0 {
+            print_lines(vec![
+                "",
+                "---------------------------------------------------",
+                "You lost all of your chips! Come back later, chump!",
+                "---------------------------------------------------",
+            ]);
+            process::exit(0);
+        }
+
         input_buffer.clear();
-        print!("> ");
+        print!("{} ", PROMPT);
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut input_buffer).expect("Failed to read input!");
         let tokens = input_buffer.trim().split(' ').collect::<Vec<&str>>();
@@ -77,9 +99,11 @@ fn main() {
         match phase {
             GamePhase::OutOfGame => {
                 match tokens[0] {
+                    "chips" => { println!("{}", chips); },
                     "exit" => { process::exit(0); },
                     "help" => {
                         print_help_menu(vec![
+                            "chips: Show how many chips you have left.",
                             "exit: End the game.",
                             "start <wager>: Start a new hand with the given wager.",
                         ]);
@@ -105,28 +129,21 @@ fn main() {
                             continue;
                         }
 
+                        bet = wager;
                         chips -= wager;
                         phase = GamePhase::InGame;
                         deck = build_deck(&mut rng);
+                        hand.clear();
 
+                        // TODO: Handle blackjack off the draw.
                         let down = deck.pop().unwrap();
                         let up = deck.pop().unwrap();
                         hand.push(down.clone());
                         hand.push(up.clone());
 
-                        print_lines(vec![
-                            "You have been dealt the following cards:",
-                            format!("(down) {}", get_card_name(&down)).as_str(),
-                            format!("(up) {}", get_card_name(&up)).as_str(),
-                            "",
-                            format!("Your hand value is {}!", get_hand_value(&hand)).as_str(),
-                            "",
-                            "You can now choose from the following actions:",
-                            "- hit: Have the dealer give you another card. Don't go over 21, though!",
-                            "- stay: Keep your current hand value and let the dealer play.",
-                            "",
-                            "To view more options, you can type \"help\".",
-                        ]);
+                        println!("You have been dealt the following cards:");
+                        print_hand(&hand);
+                        println!("\nTo view available commands, type \"help\".");
                     },
                     _ => println!("Invalid command!"),
                 };
@@ -134,9 +151,11 @@ fn main() {
             GamePhase::InGame => {
                 match tokens[0] {
                     "exit" => { process::exit(0); },
+                    "hand" => { print_hand(&hand); },
                     "help" => {
                         print_help_menu(vec![
                             "exit: End the game.",
+                            "hand: View the cards you currently have in your hand.",
                             "hit: Have the dealer give you another card. Don't go over 21, though!",
                             "leave: Leave the current hand."
                         ]);
@@ -144,12 +163,23 @@ fn main() {
                     "hit" => {
                         let card = deck.pop().unwrap();
                         hand.push(card.clone());
-                        print_lines(vec![
-                            format!("You have been dealt the {}!", get_card_name(&card)).as_str(),
-                            format!("Your hand value is now {}!", get_hand_value(&hand)).as_str(),
-                        ]);
+                        println!("You have been dealt the {}!\n", get_card_name(&card));
+                        print_hand(&hand);
+
+                        let hand_value = get_hand_value(&hand);
+                        if hand_value > 21 {
+                            println!("\nYOU BUSTED!!!");
+                            phase = GamePhase::OutOfGame;
+                        } else if hand_value == 21 {
+                            println!("\nBLACKJACK!");
+                            chips += bet;
+                            phase = GamePhase::OutOfGame;
+                        }
                     },
-                    "leave" => { phase = GamePhase::OutOfGame; },
+                    "leave" => {
+                        phase = GamePhase::OutOfGame;
+                        println!("You have quit your hand!");
+                    },
                     _ => println!("Invalid command!"),
                 }
             },
